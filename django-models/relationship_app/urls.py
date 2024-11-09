15332e727496a1319de1fc01f3c68c3ac93f9cc3
MarwanIssa100 <missa4312@gmail.com>
2024-11-09 22:27:20 +0200

    edit new secound begining

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import LibraryDetailView , list_books
+from .views import list_books
+from .views import LibraryDetailView 
 
 urlpatterns = [
     path('library/', LibraryDetailView.as_view(), name='library-detail'),
