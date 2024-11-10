56e682038aa8f1899712c180ee1f4f64077d8871
MarwanIssa100 <missa4312@gmail.com>
2024-11-10 21:10:05 +0200

    edit new begininng

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -24,6 +24,6 @@ class LibraryDetailView(DetailView):
 
 
 class register(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm() 
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
