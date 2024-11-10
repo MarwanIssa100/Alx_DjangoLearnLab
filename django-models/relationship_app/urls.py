b9a1143b87f05dbc40a25cea95f942b19694f409
MarwanIssa100 <missa4312@gmail.com>
2024-11-10 23:09:23 +0200

    edit new begininng

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('library/', LibraryDetailView.as_view(), name='library-detail'),
     path('list_books/', list_books, name='list_books'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.views.generic import CreateView
 from django.contrib.auth import login
 from .models import Library
 from .models import Book 
+from django.contrib.auth.decorators import user_passes_test
 
 def list_books(request):
     books = Book.objects.all() 
@@ -27,3 +28,29 @@ class register(CreateView):
     form_class = UserCreationForm() 
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+    
+    
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    # This view is only accessible to users with the 'Admin' role
+    return render(request, 'admin_view.html')  # You can customize the template as needed
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    # This view is only accessible to users with the 'Librarian' role
+    return render(request, 'librarian_view.html') 
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    # This view is only accessible to users with the 'Member' role
+    return render(request, 'member_view.html')
