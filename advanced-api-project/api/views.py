a3e6e2ff896904bd6cf4341808bad8173dc4be5d
MarwanIssa100 <missa4312@gmail.com>
2024-11-29 20:58:58 +0200

    edit task 2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,7 @@ from .serializers import BookSerializer, AuthorSerializer
 from .models import Book, Author
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated 
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter , OrderingFilter
+from rest_framework import filters
 from django_filters import rest_framework
 
 # Create your views here.
@@ -12,7 +12,7 @@ class ListView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend , SearchFilter , OrderingFilter]
+    filter_backends = [DjangoFilterBackend , filters.SearchFilter , filters.OrderingFilter]
     filterset_fields = ['title', 'author']
     search_fields = ['title', 'author']
     orderinf_fields = ['title', 'publication_year']
