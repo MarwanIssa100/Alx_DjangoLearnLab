0fa72a0607e76dc7bf341dfbab840874bda7ba19
MarwanIssa100 <missa4312@gmail.com>
2024-11-29 19:52:15 +0200

    edit task 1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import render
 from rest_framework import generics , viewsets
 from .serializers import BookSerializer, AuthorSerializer
 from .models import Book, Author
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated ,IsAuthenticatedOrReadOnly
 
 # Create your views here.
 class ListView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
     
     
 class DetailView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = 'id'
+    permission_classes = [IsAuthenticatedOrReadOnly]
     
 class CreateView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
