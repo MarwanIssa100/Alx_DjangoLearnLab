c9b29d2f4bd80df42a36d7e1ec61c9d73a03addc
MarwanIssa100 <missa4312@gmail.com>
2024-12-07 00:20:39 +0200

    edit task 5

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,5 +18,7 @@ urlpatterns = [
     path('posts/<int:post_id>/comments/', views.CommentListView.as_view() , name='comment_list'),
     path ('comment/<int:pk>/update/', views.CommentUpdateView.as_view() , name='comment_update'),
     path ('comment/<int:pk>/delete/', views.CommentDeleteView.as_view() , name='comment_delete'),
-        path('search/', views.PostSearchView.as_view(), name='post_search'),
+    path('search/', views.PostSearchView.as_view(), name='post_search'),
+    path('tags/<slug:tag_slug>/',views.PostByTagListView,name='tags'),
+        
 ]
