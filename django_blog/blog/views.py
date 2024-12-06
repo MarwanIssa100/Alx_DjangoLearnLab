cabf8d0e231465a7b84f9adb20884827c17ad04e
MarwanIssa100 <missa4312@gmail.com>
2024-12-06 22:08:09 +0200

    task 4

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from .forms import SignUpForm , LoginForm , ProfileForm , PostForm ,CommentForm
 from django.views.generic import ListView , DetailView , CreateView ,UpdateView , DeleteView
 from .models import Post , Comment
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy , reverse
 from django.contrib.auth.mixins import LoginRequiredMixin ,UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 
@@ -124,19 +124,67 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         post = self.get_object()
         return post.author == self.request.user
     
-def CommentCreateView(request , pk):
-    post = get_object_or_404(Post , pk = pk)
-    if request.method == 'POST':
-        comment = CommentForm(request.POST)
-        if comment.is_valid():
-            #we should make (commit = False) to let django set the user and post ids before saving the comment form  
-            comment = comment.save(commit = False)
-            comment.author = request.user
-            comment.post = post
-            comment.save()
-            return redirect('post_detail' , pk = post.pk)
+# def create_comment(request , pk):
+#     post = get_object_or_404(Post , pk = pk)
+#     if request.method == 'POST':
+#         comment = CommentForm(request.POST)
+#         if comment.is_valid():
+#             #we should make (commit = False) to let django set the user and post ids before saving the comment form  
+#             comment = comment.save(commit = False)
+#             comment.author = request.user
+#             comment.post = post
+#             comment.save()
+#             return redirect('post_detail' , pk = post.pk)
             
-    else:
-        comment = CommentForm()
-    return render(request , 'blog/comment_form.html' , {'comment':comment , 'post':post})
-            
+#     else:
+#         comment = CommentForm()
+#     return render(request , 'blog/comment_form.html' , {'comment':comment , 'post':post})
+
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+    
+    def form_valid(self , form):
+        post = get_object_or_404(Post , id = self.kwargs['post_id'])
+        form.instance.author = self.request.user
+        form.instance.post = post
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        post_id = self.kwargs['post_id']
+        return reverse('post_detail' , kwargs = {'post_id':post_id})
+    
+class CommentListView(ListView):
+    model = Comment
+    template_name = 'blog/comments.html'
+    context_object_name = 'comments'    
+    
+    def get_queryset(self):
+        post = get_object_or_404(Post , id = self.kwargs['post_id'])
+        return Comment.objects.filter(post = post)
+    
+    def get_context_data(self , **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post'] = get_object_or_404(Post , id = self.kwargs['post_id'])
+        return context
+    
+class CommentUpdateView(UpdateView , LoginRequiredMixin , UserPassesTestMixin):
+    model = Comment
+    fields = ['content']
+    template_name = 'blog/comment_update.html'
+    success_url = reverse_lazy('blog')
+    
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
+    
+class CommentDeleteView(DeleteView , LoginRequiredMixin , UserPassesTestMixin):
+    model = Comment
+    template_name = 'blog/comment_confirm_delete.html'
+    success_url = reverse_lazy('blog')
+    
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
+    
