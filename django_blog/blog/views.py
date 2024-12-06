ff3032aadeafa862e516cbec6bec153de3498acb
MarwanIssa100 <missa4312@gmail.com>
2024-12-06 02:46:33 +0200

    task 3

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import login , logout , authenticate
 from django.contrib import messages
-from .forms import SignUpForm , LoginForm , ProfileForm
+from .forms import SignUpForm , LoginForm , ProfileForm , PostForm
+from django.views.generic import ListView , DetailView , CreateView ,UpdateView , DeleteView
+from .models import Post
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin ,UserPassesTestMixin
+
 
 # Create your views here.
 
@@ -53,3 +58,61 @@ def logout_view(request):
     logout(request)
     messages.success(request, "Logged out successfully!")
     return redirect('login')
+
+class ListView(ListView):
+    model = Post
+    template_name = 'blog/blogs.html'
+    context_object_name = 'posts'
+    
+    def get_queryset(self):
+        return Post.objects.all()
+    
+class DetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_details.html'
+    context_object_name = 'post'
+    
+    def get_queryset(self):
+        return Post.objects.filter(id = self.kwargs['pk'])
+
+class CreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('blog')
+    
+    def form_valid(self , form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']  # Fields to display in the form
+    template_name = 'blog/post_update.html'  # Template for the update form
+    success_url = reverse_lazy('blog')  # Redirect to post details after successful update
+
+    def form_valid(self, form):
+        """
+        Additional logic (if needed) before saving the form.
+        """
+        form.instance.author = self.request.user  # Ensure the author remains the same
+        return super().form_valid(form)
+
+    def test_func(self):
+        """
+        Restrict access to the original author of the post.
+        """
+        post = self.get_object()
+        return post.author == self.request.user
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'  # Template for confirmation
+    success_url = reverse_lazy('blog')  # Redirect to the post list after deletion
+
+    def test_func(self):
+        """
+        Restrict delete access to the author of the post.
+        """
+        post = self.get_object()
+        return post.author == self.request.user
